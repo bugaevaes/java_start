cef3fe776740af1faa883d813064cb6a0a27f81e
Evgeniya <josya.bu@gmail.com>
2016-08-22 00:16:40 +0700

    создание метода расчета длины

--- sandbox/src/main/java/homework/Execute.java
+++ sandbox/src/main/java/homework/Execute.java
@@ -8,14 +8,7 @@ public class Execute {
         Point p2 = new Point();
         p2.x = 7.0;
         p2.y = 8.0;
-        System.out.println("Расстояние между точками равно " + distance(p1, p2));
-    }
-
-    public static double distance(Point p1, Point p2) {
-        double lx = p2.x - p1.x;
-        double ly = p2.y - p1.y;
-        double z = Math.pow(lx, 2) + Math.pow(ly, 2);
-        return Math.sqrt(z);
+        System.out.println("Расстояние между точками равно " + p1.distance(p2));
 
     }
 }
--- sandbox/src/main/java/homework/Point.java
+++ sandbox/src/main/java/homework/Point.java
@@ -4,4 +4,13 @@ public class Point {
 
     public double x;
     public double y;
-}
+
+    public double distance(Point p) {
+        double lx = p.x - this.x;
+        double ly = p.y - this.y;
+        double z = Math.pow(lx, 2) + Math.pow(ly, 2);
+        return Math.sqrt(z);
+
+    }
+
+   }
