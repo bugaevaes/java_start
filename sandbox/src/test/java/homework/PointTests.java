7c1cf72f38038aa792d054ddbc4a7c73c647dd7b
Evgeniya <josya.bu@gmail.com>
2016-08-25 00:04:57 +0700

    Добавлены тесты для класса Point

--- sandbox/src/test/java/homework/PointTests.java
+++ sandbox/src/test/java/homework/PointTests.java
@@ -1,6 +1,21 @@
 package homework;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class PointTests {
+    @Test
+    public void testDistance1() {
+        Point p1 = new Point(1.0, 1.0);
+        Point p2 = new Point(4.0, 5.0);
+        Assert.assertEquals(p1.distance(p2), 5.0);
 
+    }
+    @Test
+    public void testDistance2() {
+        Point p1 = new Point(2.0, -1.0);
+        Point p2 = new Point(6.0, 2.0);
+        Assert.assertEquals(p2.distance(p1), 5.0);
+
+    }
 }
