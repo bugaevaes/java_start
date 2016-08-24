43b2bc06e957a325a5bb17d8ea8574a0dafd1422
Evgeniya <josya.bu@gmail.com>
2016-08-24 23:41:11 +0700

    добавлен конструктор для класса Point; добавлен Testng

--- sandbox/src/main/java/homework/Execute.java
+++ sandbox/src/main/java/homework/Execute.java
@@ -2,12 +2,8 @@ package homework;
 
 public class Execute {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        p1.x = 4.0;
-        p1.y = 4.0;
-        Point p2 = new Point();
-        p2.x = 7.0;
-        p2.y = 8.0;
+        Point p1 = new Point(4.0, 4.0);
+        Point p2 = new Point(7.0, 8.0);
         System.out.println("Расстояние между точками равно " + p1.distance(p2));
 
     }
--- sandbox/src/main/java/homework/Point.java
+++ sandbox/src/main/java/homework/Point.java
@@ -5,6 +5,11 @@ public class Point {
     public double x;
     public double y;
 
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
     public double distance(Point p) {
         double lx = p.x - this.x;
         double ly = p.y - this.y;
