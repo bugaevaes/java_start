d68c9a3dfd30b373248ba6b66d5a6e9d068e1265
boogvar <boogvar@ya.ru>
2016-09-06 00:08:41 +0700

    Добавлена возможность запуска под разными браузерами

--- addressbook-web-tests/src/test/java/test/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/test/addressbook/appmanager/NavigationHelper.java
@@ -2,12 +2,13 @@ package test.addressbook.appmanager;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase {
 
 
-    public NavigationHelper(FirefoxDriver wd) {
+    public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
