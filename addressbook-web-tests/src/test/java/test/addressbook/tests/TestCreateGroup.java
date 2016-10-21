722ee8c84ea7e3a5ad59338a64a9ae32ee6ae1a8
bugaevaes <josya.bu@gmail.com>
2016-10-21 17:02:06 +0700

    [Addressbook] Добавлена возможность логирования

--- addressbook-web-tests/src/test/java/test/addressbook/tests/TestCreateGroup.java
+++ addressbook-web-tests/src/test/java/test/addressbook/tests/TestCreateGroup.java
@@ -1,6 +1,8 @@
 package test.addressbook.tests;
 
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import test.addressbook.model.GroupData;
