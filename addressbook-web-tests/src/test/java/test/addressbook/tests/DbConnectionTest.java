0e4f19d8d1078d452b16be60b9e4db1849a43b41
Evgeniya <josya.bu@gmail.com>
2016-11-06 19:41:00 +0700

    Работа с БД - 2

---
+++ addressbook-web-tests/src/test/java/test/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,43 @@
+package test.addressbook.tests;
+
+import org.testng.annotations.Test;
+import test.addressbook.model.GroupData;
+import test.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+
+    @Test
+
+    public void testDbConnection() {
+
+        Connection conn = null;
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/test?" +
+                            "user=root&password=");
+
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select group_id, group_name, group_header from group_list");
+            Groups groups = new Groups();
+            while (rs.next()){
+                groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                        .withHeader(rs.getString("group_header")));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+
+            System.out.println(groups);
+
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
+    }
+}
